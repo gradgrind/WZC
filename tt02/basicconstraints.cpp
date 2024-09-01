d5124c3bd6593ff87dca517bbace58f2fd77d1ae
gradgrind <mt.bothfeld@gmail.com>
2024-09-01 11:38:31 +0200

    basic-constraints: initial placements

--- tt02/basicconstraints.cpp
+++ tt02/basicconstraints.cpp
@@ -12,11 +12,13 @@ BasicConstraints::BasicConstraints(DBData *dbdata) : db_data{dbdata}
     int sgi = 0;
     for (int gid : dbdata->Tables.value("GROUPS")) {
         auto node = dbdata->Nodes.value(gid).DATA;
-        if (node.value("ID").toString().isEmpty()) {
-            // A whole-class group
-            auto sglist = node.value("SUBGROUPS").toArray();
-            for (auto sg : sglist) {
-                auto sgstr = sg.toString();
+        auto sglist = node.value("SUBGROUPS").toArray();
+        bool allsg = node.value("ID").toString().isEmpty();
+        for (auto sg : sglist) {
+            auto sgstr = sg.toString();
+            g2sg[gid].append(sgstr);
+            if (allsg) {
+                // A whole-class group
                 sg2i[sgstr] = sgi;
                 sgi++;
             }
@@ -39,13 +41,12 @@ BasicConstraints::BasicConstraints(DBData *dbdata) : db_data{dbdata}
     */
 
     // Make a weekly array for each teacher
-    int ti = 0;
     for (int tid : dbdata->Tables.value("TEACHERS")) {
-        t2i[tid] = ti;
-        ti++;
+        t2i[tid] = i_t.length();
+        i_t.append(tid);
     }
     t_weeks = std::vector<std::vector<std::vector<int>>>(
-        ti, std::vector<std::vector<int>> (
+        i_t.length(), std::vector<std::vector<int>> (
             ndays , std::vector<int> (nhours)));
     /*
     qDebug() << "t_weeks:"
@@ -58,17 +59,17 @@ BasicConstraints::BasicConstraints(DBData *dbdata) : db_data{dbdata}
     int ri = 0;
     for (int rid : dbdata->Tables.value("ROOMS")) {
         auto node = dbdata->Nodes.value(rid).DATA;
-        if (!node.contains("SUBROOMS")) {
-            // A real room
-            r2i[rid] = ri;
-            ri++;
-        }
+        if (node.contains("SUBROOMS")) continue;
+        // A real room
+        r2i[rid] = ri;
+        ri++;
     }
     r_weeks = std::vector<std::vector<std::vector<int>>>(
         ri, std::vector<std::vector<int>> (
             ndays , std::vector<int> (nhours)));
 
     slot_blockers();
+    place_lessons();
 }
 
 void BasicConstraints::slot_blockers()
@@ -97,6 +98,104 @@ void BasicConstraints::slot_blockers()
             int h = db_data->hours.value(bpair.at(1).toInt());
             for (auto sg : sglist) {
                 sg_weeks.at(sg2i.value(sg.toString())).at(d).at(h) = -1;
+            }
+        }
+    }
+    // Block slots where rooms are "not available"
+    for (int rid : db_data->Tables.value("ROOMS")) {
+        auto node = db_data->Nodes.value(rid).DATA;
+        if (node.contains("SUBROOMS")) continue;
+        auto blist = node.value("NOT_AVAILABLE").toArray();
+        for (auto b : blist) {
+            auto bpair = b.toArray();
+            int d = db_data->days.value(bpair.at(0).toInt());
+            int h = db_data->hours.value(bpair.at(1).toInt());
+            //qDebug() << db_data->get_tag(tid) << d << h;
+            t_weeks.at(r2i.value(rid)).at(d).at(h) = -1;
+        }
+    }
+}
+
+// Initial placement of the lessons.
+void BasicConstraints::place_lessons()
+{
+    for (int cid : db_data->Tables.value("COURSES")) {
+        lesson_data ldc;
+        auto node = db_data->Nodes.value(cid).DATA;
+        auto glist = node.value("STUDENTS").toArray();
+        for(auto g : glist) {
+            for (const auto &sg : g2sg.value(g.toInt())) {
+                ldc.groups.push_back(sg2i.value(sg));
+            }
+        }
+        auto tlist = node.value("TEACHERS").toArray();
+        for (auto t : tlist) {
+            ldc.teachers.push_back(t2i.value(t.toInt()));
+        }
+        //TODO: Need the possible-rooms-list (in "ROOMSPEC")
+        auto rlist = node.value("ROOMSPEC").toArray();
+        for (auto rlv : rlist) {
+            std::vector<int> rilist;
+            auto rl = rlv.toArray();
+            for (auto r : rl) {
+                rilist.push_back(r2i.value(r.toInt()));
+            }
+            ldc.roomspec.push_back(rilist);
+        }
+        // The used rooms are associated with the individual lessons.
+        // Note that they are only valid if there is a slot placement.
+        for (int lid : db_data->course_lessons.value(cid)) {
+            lesson_data ld(ldc);
+            auto lnode = db_data->Nodes.value(lid).DATA;
+            auto rlist = lnode.value("ROOMS").toArray();
+            for (auto r : rlist) {
+                ld.rooms.push_back(r2i.value(r.toInt()));
+            }
+            lesson_resources[lid] = ld;
+            int d0 = lnode.value("DAY").toInt();
+            if (d0 == 0) continue;
+            int d = db_data->days.value(d0);
+            int h = db_data->hours.value(lnode.value("HOUR").toInt());
+            int l = lnode.value("LENGTH").toInt();
+            //TODO: What to do with "FIXED"?
+            // Test placement before actually doing it
+            for (int i = 0; i < l; i++) {
+                int hh = h + i;
+                for (int t : ld.teachers) {
+                    if (t_weeks.at(t).at(d).at(hh) != 0) {
+                        qFatal() << "Couldn't place lesson" << lid
+                                 << "// teacher" << i_t.at(t) << "in"
+                                 << t_weeks.at(t).at(d).at(hh)
+                                 << "@ Slot" << d << h;
+                    }
+                }
+                for (int sg : ld.groups) {
+                    if (sg_weeks.at(sg).at(d).at(hh) != 0) {
+                        qFatal() << "Couldn't place lesson" << lid
+                                 << "// group in"
+                                 << sg_weeks.at(sg).at(d).at(hh);
+                    }
+                }
+                for (int r : ld.rooms) {
+                    if (r_weeks.at(r).at(d).at(hh) != 0) {
+                        qFatal() << "Couldn't place lesson" << lid
+                                 << "// room in"
+                                 << r_weeks.at(r).at(d).at(hh);
+                    }
+                }
+            }
+            // Now do the placement
+            for (int i = 0; i < l; i++) {
+                int hh = h + i;
+                for (int t : ld.teachers) {
+                    t_weeks.at(t).at(d).at(hh) = lid;
+                }
+                for (int sg : ld.groups) {
+                    sg_weeks.at(sg).at(d).at(hh) = lid;
+                }
+                for (int r : ld.rooms) {
+                    r_weeks.at(r).at(d).at(hh) = lid;
+                }
             }
         }
     }
--- tt02/basicconstraints.h
+++ tt02/basicconstraints.h
@@ -4,6 +4,13 @@
 #include "database.h"
 #include "with_roaring/roaring.hh"
 
+struct lesson_data{
+    std::vector<int> teachers;
+    std::vector<int> groups;
+    std::vector<int> rooms;
+    std::vector<std::vector<int>> roomspec;
+};
+
 class BasicConstraints
 {
 public:
@@ -12,15 +19,28 @@ public:
     DBData * db_data;
     int ndays;
     int nhours;
+    // group index -> list of atomic subgroups:
+    QHash<int, QList<QString>> g2sg;
+    // atomic subgroup -> week-block index:
     QHash<QString, int> sg2i;
+    // week-blocks for atomic subgroups:
     std::vector<std::vector<std::vector<int>>> sg_weeks;
+    // teacher index -> week-block index:
     QHash<int, int> t2i;
+    // week-block index -> teacher index:
+    QList<int> i_t;
+    // week-blocks for teachers:
     std::vector<std::vector<std::vector<int>>> t_weeks;
+    // room index -> week-block index:
     QHash<int, int> r2i;
+    // week-blocks for rooms:
     std::vector<std::vector<std::vector<int>>> r_weeks;
+    // placement resources for each lesson
+    std::unordered_map<int, lesson_data> lesson_resources;
 
 private:
     void slot_blockers();
+    void place_lessons();
 };
 
 #endif // BASICCONSTRAINTS_H
